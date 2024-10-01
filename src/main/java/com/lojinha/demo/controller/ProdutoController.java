ed6b98f0b7dc7c26c1f7cbe5309bc6041a0f9358
lihbolivia <116227611+lihbolivia@users.noreply.github.com>
2024-10-01 14:10:54 -0300

    insert

--- src/main/java/com/lojinha/demo/controller/ProdutoController.java
+++ src/main/java/com/lojinha/demo/controller/ProdutoController.java
@@ -1,6 +1,8 @@
 package com.lojinha.demo.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.lojinha.demo.model.Produto;
 
 @RestController
 public class ProdutoController {
--- src/main/java/com/lojinha/demo/model/Produto.java
+++ src/main/java/com/lojinha/demo/model/Produto.java
@@ -1,13 +1,17 @@
 package com.lojinha.demo.model;
 
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import javax.swing.Spring;
 
 public class Produto {
 
     private int id;
     private double valor;
-    private Spring nome;
+    private String nome;
     public int getId() {
         return id;
     }
@@ -20,11 +24,37 @@ public class Produto {
     public void setValor(double valor) {
         this.valor = valor;
     }
-    public Spring getNome() {
+    public String getNome() {
         return nome;
     }
-    public void setNome(Spring nome) {
+    public void setNome(String nome) {
         this.nome = nome;
     }
+
+     public void insert() {
+        Conexao c = new Conexao();
+        Connection dbConn = (Connection) c.getConexao();
+
+        String sql = "INSERT INTO produto "+
+        "(valor, nome) "+
+        "VALUES (?, ?)";
+
+        
+        try {
+            PreparedStatement pstmt = dbConn.prepareStatement(sql);
+
+            pstmt.setDouble(3, this.valor);
+            pstmt.setString(2, this.nome);
+         
+
+            pstmt.executeUpdate();
+
+    
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
     
 }
